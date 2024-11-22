b49d1a9c38f20363dcf7495a753c8a510120d23e
kpzip <kpzipgames@gmail.com>
2024-11-21 23:47:16 -0800

    initialize project

---
+++ program/Ornament Argb/Ornament Argb/main.c
@@ -0,0 +1,18 @@
+/*
+ * Ornament Argb.c
+ *
+ * Created: 11/21/2024 11:37:21 PM
+ * Author : kpzip
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
