895718c8a4863c08a704badde0ed8a23270d5436
kpzip <kpzipgames@gmail.com>
2024-12-08 11:12:34 -0800

    progress

--- program/Ornament Argb/Ornament Argb/main.c
+++ program/Ornament Argb/Ornament Argb/main.c
@@ -8,6 +8,9 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
+#define nop asm("nop");
+#define sbi(x,b) (x) |= 1<<(b)
+#define cbi(x,b) (x) &= ~(1<<(b))
 
 int main(void)
 {
@@ -24,7 +27,43 @@ typedef struct Color {
 
 void show(Color* colors, int colors_len) {
 	cli
-	asm("nop")
+	nop
+	sei
+}
+
+inline void writeOne() {
+	// Might want to use hardware timers instead of nops in the future
+	// T1H = 0.6 +/- 0.15 uS -> 10 clock cycles
+	// T1L = 0.6 +/- 0.15 uS -> 10 clock cycles
+	
+	// Set high - Low is default
+	sbi(DDRB, 5)
+	
+	cli
+	nop
+	nop
+	nop
+	nop
+	
+	nop
+	nop
+	nop
+	nop
+	nop
+	
+	// Set low
+	cbi(DDRB, 5)
+	
+	//nop
+	nop
+	nop
+	nop
+	nop
+	
+	nop
+	nop
+	nop
+	nop
 	sei
 }
 
