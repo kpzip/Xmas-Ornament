c950236cf6893f2f0bd037f5132ae0445d5ea636
kpzip <kpzipgames@gmail.com>
2024-12-09 00:18:31 -0800

    :(

--- program/Ornament Argb/Ornament Argb/main.c
+++ program/Ornament Argb/Ornament Argb/main.c
@@ -8,7 +8,7 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-#define nop asm("nop");
+#define nop __asm__ volatile("nop");
 
 // IMPORTANT: These should compile to one instruction for timing purposes. Luckily the avr gcc makers knew what they were doing!
 #define sbi(x,b) (x) |= 1<<(b)
@@ -16,48 +16,41 @@
 
 #define LED_PIN 6
 #define SWITCH_PIN 5
+#define STRIP_LEN 10
 
 typedef struct Color {
 	unsigned char r, g, b;
 } Color;
 
-static Color strip[10];
+static Color strip[STRIP_LEN];
 
 int main(void)
 {
-	writeAllZeros();
-	writeAllOnes();
-	writeAllZeros();
+	setup();
+	memset(strip, 128, sizeof(Color) * STRIP_LEN);
+	Color red = { .r = 128, .g = 0, .b = 0 };
+	strip[0] = red;
+	//writeByte(0);
+	show(strip, 1);
     /* Replace with your application code */
     while (1) 
     {
+		//sbi(PORTB, LED_PIN);
+		//cbi(PORTB, LED_PIN);
 		nop
-    }
+	}
 }
 
-void writeAllOnes() {
-	writeOne();
-	writeOne();
-	writeOne();
-	writeOne();
-	writeOne();
-	writeOne();
-	writeOne();
-	writeOne();
+inline void setup() {
+	// Set LED Pin as output
+	sbi(DDRB, LED_PIN);
+	// Set program change switch pin as input and engage pull-up resistor
+	cbi(DDRB, SWITCH_PIN);
+	//
+	
 }
 
-void writeAllZeros() {
-	writeZero();
-	writeZero();
-	writeZero();
-	writeZero();
-	writeZero();
-	writeZero();
-	writeZero();
-	writeZero();
-}
-
-void show(Color* colors, int colors_len) {
+void show(Color *colors, int colors_len) {
 	cli();
 	for (int i = 0; i < colors_len; i++) {
 		writeByte(colors[i].g);
@@ -87,7 +80,7 @@ inline void writeOne() {
 	// T1L = 0.6 +/- 0.15 uS -> 10 clock cycles
 	
 	// Set high - Low is default
-	sbi(DDRB, 5);
+	sbi(PORTB, LED_PIN);
 	
 	nop
 	nop
@@ -98,23 +91,23 @@ inline void writeOne() {
 	nop
 	nop
 	nop
-	nop
-	nop
+	//nop
+	//nop
 	
 	// Set low
-	cbi(DDRB, LED_PIN);
+	cbi(PORTB, LED_PIN);
 	
 	//nop
 	nop
 	nop
-	nop
-	nop
+	//nop
+	//nop
 	
-	nop
-	nop
-	nop
-	nop
-	nop
+	//nop
+	//nop
+	//nop
+	//nop
+	//nop
 	
 	// Leave it as low
 }
@@ -126,16 +119,16 @@ inline void writeZero() {
 	// T0L = 0.9 +/- 0.15 uS -> 15 clock cycles
 	
 	// Set high - Low is default
-	sbi(DDRB, LED_PIN);
+	sbi(PORTB, LED_PIN);
 	
 	nop
 	nop
 	nop
-	nop
-	nop
+	//nop
+	//nop
 	
 	// Set low
-	cbi(DDRB, LED_PIN);
+	cbi(PORTB, LED_PIN);
 	
 	//nop
 	nop
@@ -146,14 +139,14 @@ inline void writeZero() {
 	nop
 	nop
 	nop
-	nop
-	nop
+	//nop
+	//nop
 	
-	nop
-	nop
-	nop
-	nop
-	nop
+	//nop
+	//nop
+	//nop
+	//nop
+	//nop
 	
 	// Leave it as low
 }
@@ -161,7 +154,7 @@ inline void writeZero() {
 // Send reset code
 // Interrupts *can* be enabled since longer is fine
 inline void resetCode() {
-	cbi(DDRB, LED_PIN);
+	cbi(PORTB, LED_PIN);
 	for (int i = 0; i > 1000; i++) {
 		nop
 	}
